4fe691bac3809c5cd0477fb236ac15bd6019f08a
Nakusuo <rodriguezmarcela0000@gmail.com>
2025-11-26 15:28:44 -0500

    Fix: Deshabilitar healthcheck temporalmente

--- backend/start-app.bat
+++ backend/start-app.bat
@@ -1,3 +1,6 @@
 @echo off
 cd /d %~dp0
-mvnw.cmd spring-boot:run
+echo ====================================
+echo Iniciando aplicación en modo LOCAL
+echo ====================================
+mvnw.cmd spring-boot:run -Dspring-boot.run.profiles=dev
