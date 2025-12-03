5bf8ec9168ab78d0aab99c6974029c4868506c5b
Nakusuo <rodriguezmarcela0000@gmail.com>
2025-12-03 12:29:49 -0500

    aaa

--- backend/src/main/java/com/pnp/mesadepartes/model/HojaTramite.java
+++ backend/src/main/java/com/pnp/mesadepartes/model/HojaTramite.java
@@ -1,6 +1,7 @@
 package com.pnp.mesadepartes.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
